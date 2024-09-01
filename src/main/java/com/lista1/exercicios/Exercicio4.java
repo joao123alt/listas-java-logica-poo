c4296ff14359ee929a0ba3eb3d539bc121e695bc
joao123alt <joaocasadinho324@gmail.com>
2024-09-01 20:33:47 -0300

    Adicionado exercicio 4

---
+++ src/main/java/com/lista1/exercicios/Exercicio4.java
@@ -0,0 +1,18 @@
+package com.lista1.exercicios;
+
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void resolucao() {
+    
+        int numero1;
+
+        System.out.println("Digite um numero: ");
+        Scanner leia = new Scanner(System.in);
+        numero1 = leia.nextInt();
+
+        System.out.println("O antecessor é " + (numero1 - 1));
+        System.out.println("O sucessor é: " + (numero1 + 1));
+    }
+}
