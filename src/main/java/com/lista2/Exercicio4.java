33c17f9176de892564bbee4e062401bde191aa70
joao123alt <joaocasadinho324@gmail.com>
2024-09-02 18:43:02 -0300

    Adicionando exercicio 4

---
+++ src/main/java/com/lista2/Exercicio4.java
@@ -0,0 +1,31 @@
+package com.lista2;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void resolucao2(){
+      //Código de resolução: Exercicio4.resolucao2();
+         Scanner sc = new Scanner(System.in);
+         String condicao, nome;
+         int idade;
+
+         System.out.println("Nome: ");
+         nome = sc.nextLine();
+
+         System.out.println("Idade: ");
+         idade = sc.nextInt();
+
+         if(idade >= 16 && idade <= 18){
+            System.out.println("Acesso somente acompanhado pelo responsável.");
+         }
+         else if(idade > 18){
+            System.out.println("Acesso Liberado.");
+         }
+         else{
+            System.out.println("Acesso Negado.");
+         }
+
+         sc.close();
+    }     
+
+}
